6de1cb10b5a9a4b20f98936569c68e3f7df4a1a6
yanglebupt <yanglebupt@qq.com>
2025-01-19 19:11:45 +0800

    modify main.test.ts

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,22 +1,24 @@
-import * as XLSX from "xlsx";
-import { label_name, usedHeaders_list } from "./app/headers";
-import { predict, requestBuffer } from "./tool";
-// import filepath from "./data0130/0126validation2.xlsx";
-import filepath from "./data0130/0126training.xlsx";
+// import * as XLSX from "xlsx";
+// import { label_name, usedHeaders_list } from "./app/headers";
+// import { predict, requestBuffer } from "./tool";
+// // import filepath from "./data0130/0126validation2.xlsx";
+// import filepath from "./data0130/0126training.xlsx";
 
-const excelBuffer = await requestBuffer(filepath);
-const workbook = XLSX.read(excelBuffer, {
-  type: "buffer",
-});
-const worksheetJson = XLSX.utils.sheet_to_json(workbook.Sheets["Sheet1"]);
+// const excelBuffer = await requestBuffer(filepath);
+// const workbook = XLSX.read(excelBuffer, {
+//   type: "buffer",
+// });
+// const worksheetJson = XLSX.utils.sheet_to_json(workbook.Sheets["Sheet1"]);
 
-usedHeaders_list.slice(0, 1).forEach(async ([name, usedHeaders, _, path]) => {
-  console.log(name);
-  const probs = await predict(worksheetJson as any, usedHeaders, path, [
-    worksheetJson.length,
-    usedHeaders.length,
-  ]);
-  const labels = worksheetJson.map((record: any) => record[label_name]);
-  console.log(labels.slice(0, 10));
-  console.log(probs.slice(0, 10));
-});
+// usedHeaders_list.slice(0, 1).forEach(async ([name, usedHeaders]) => {
+//   console.log(name);
+//   // const probs = await predict(worksheetJson as any, usedHeaders, path, [
+//   //   worksheetJson.length,
+//   //   usedHeaders.length,
+//   // ]);
+//   // const labels = worksheetJson.map((record: any) => record[label_name]);
+//   // console.log(labels.slice(0, 10));
+//   // console.log(probs.slice(0, 10));
+// });
+
+console.log("test")
