f5d5293fe27c9df8186dec26c120c2dda3c3230d
yanglebupt <yanglebupt@qq.com>
2025-01-21 17:51:12 +0800

    add base prefix

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "osssi",
   assetsInclude: ["**/*.xlsx", "**/*.onnx"],
   plugins: [
     react(),
