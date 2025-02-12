37230d620b8ff192721a88a2e8297e155b927506
yanglebupt <yanglebupt@qq.com>
2025-02-12 18:30:37 +0800

    base /osssi

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "osssi",
+  base: "/osssi",
   assetsInclude: ["**/*.xlsx", "**/*.onnx"],
   plugins: [
     react(),
