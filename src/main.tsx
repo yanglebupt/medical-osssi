2eb2e132f32917f8f6eaf1ace894b826d9563e95
yanglebupt <yanglebupt@qq.com>
2025-02-24 23:44:25 +0800

    if @vercel/speed-insights

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,11 @@ import ReactDOM from "react-dom/client";
 import { SpeedInsights } from "@vercel/speed-insights/react";
 import App from "./App";
 
-console.log(import.meta.env.MODE)
+console.log(import.meta.env.MODE != "production")
 
 ReactDOM.createRoot(document.getElementById("app")!).render(
   <React.StrictMode>
     <App />
-    {import.meta.env.MODE == "production" && <SpeedInsights />}
+    {import.meta.env.MODE != "production" && <SpeedInsights />}
   </React.StrictMode>
 );
