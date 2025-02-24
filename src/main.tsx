8068d766922f99bd1a281f625f41a57d5acf340d
yanglebupt <yanglebupt@qq.com>
2025-02-24 23:35:13 +0800

    cancel @vercel/speed-insights

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { SpeedInsights } from "@vercel/speed-insights/react";
+// import { SpeedInsights } from "@vercel/speed-insights/react";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("app")!).render(
   <React.StrictMode>
     <App />
-    {import.meta.env.MODE != "production" && <SpeedInsights />}
+    {/* {import.meta.env.MODE != "production" && <SpeedInsights />} */}
   </React.StrictMode>
 );
