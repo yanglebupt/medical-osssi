07d7a213a86fb405a5c201e9745d3d69784ac503
yanglebupt <yanglebupt@qq.com>
2025-02-24 10:00:20 +0800

    change vercel.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv, Plugin, PluginOption } from "vite";
 import viteCompression from 'vite-plugin-compression'
 import { viteStaticCopy } from "vite-plugin-static-copy";
+import { generateVercelJson } from './generate-vercel-json'
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
@@ -11,18 +12,19 @@ export default ({mode}) => {
     viteStaticCopy({
       targets: [
         {
-          src: "node_modules/onnxruntime-web/dist/wasm-opts/*.wasm",
+          src: `node_modules/onnxruntime-web/dist/${mode == "production" ? "wasm-opts/" : ""}*.wasm`,
           dest: ".",
         },
       ],
     })
   ]
 
-  if (mode == "production")
+  if (mode == "production" || mode == "pre-production")
     plugins.push(viteCompression({
       filter: /.(js|mjs|json|css|html|wasm|onnx)$/i,
       threshold: 100*1024, // 100 KB
       deleteOriginFile: true,
+      success: mode == "pre-production" ? () => generateVercelJson("dist") : undefined
     }))
 
   return defineConfig({
